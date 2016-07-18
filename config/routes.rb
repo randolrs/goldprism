4a79dd5097fde858ab0f1ed6e9c51455ef598566
randolrs <r.shane.randolph@gmail.com>
2016-07-18 17:36:21 -0500

    moving on to pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root "pages#home"
 
+   get '/:username/' => 'pages#profile_page', as: 'profile_page'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
