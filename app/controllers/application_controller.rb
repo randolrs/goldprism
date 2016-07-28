3c80687efa1ad03cfb2a4ff593a4af742bb5852d
randolrs <r.shane.randolph@gmail.com>
2016-07-28 12:14:44 -0500

    s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   	before_filter :new_track_for_form
 
-	before_action :configure_devise_permitted_parameters, if: :devise_controller?
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
   	protected
 
@@ -18,21 +18,27 @@ class ApplicationController < ActionController::Base
 
 	end
 
-	def configure_devise_permitted_parameters
+	def configure_permitted_parameters
 			
-		registration_params = [:email, :display_name, :first_name, :last_name, :password, :image, :password, :password_confirmation]
+		#devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :display_name, :first_name, :last_name, :password, :image, :password, :password_confirmation])
 
-		if params[:action] == 'update'
-		  devise_parameter_sanitizer.for(:account_update) { 
-		    |u| u.permit(registration_params)
-		  }
-		elsif params[:action] == 'create'
-		  devise_parameter_sanitizer.for(:sign_up) { 
-		    |u| u.permit(registration_params) 
+		devise_parameter_sanitizer.for(:sign_in)        << [:email, :display_name, :first_name, :last_name, :password, :image, :password, :password_confirmation]
+   		devise_parameter_sanitizer.for(:sign_up)        << [:email, :display_name, :first_name, :last_name, :password, :image, :password, :password_confirmation]
+   		devise_parameter_sanitizer.for(:account_update) << [:email, :display_name, :first_name, :last_name, :password, :image, :password, :password_confirmation]
 
-		  }
+		# registration_params = [:email, :display_name, :first_name, :last_name, :password, :image, :password, :password_confirmation]
 
-		end
+		# if params[:action] == 'update'
+		#   devise_parameter_sanitizer.for(:account_update) { 
+		#     |u| u.permit(registration_params)
+		#   }
+		# elsif params[:action] == 'create'
+		#   devise_parameter_sanitizer.for(:sign_up) { 
+		#     |u| u.permit(registration_params) 
+
+		#   }
+
+		# end
 
 	end
 
