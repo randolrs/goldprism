77fde4b4e4cecb87af44d605b5c6be3861b3a4c9
randolrs <r.shane.randolph@gmail.com>
2016-07-28 12:53:17 -0500

    insult

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
 
-gem 'devise'
+gem 'devise', '< 4.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,8 @@ class ApplicationController < ActionController::Base
 	def configure_permitted_parameters
 	    registration_params = [:email, :display_name, :first_name, :last_name, :password, :image, :password, :password_confirmation]
 	    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(registration_params) }
-	    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(registration_params << :current_password) }
+	    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(registration_params) }
+	    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(registration_params) }
   	end
 
 
